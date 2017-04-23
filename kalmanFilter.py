2f850a9c51454cb3922d4dc2a774a9c605e4fd4e
dr_coffee <yhyu.mail@gmail.com>
2017-04-23 14:57:06 -0400

    'modified'

--- kalmanFilter.py
+++ kalmanFilter.py
@@ -20,9 +20,9 @@ A = np.array([[1, tDelta],
 B = np.array([0.5*np.square(tDelta), tDelta])
 H = np.array([1, 0])
 
-xHat = np.zeros(sz)
-xHatMinus = np.zeros(sz)
-PMinus = np.zeros(sz)
+xHat = np.zeros(sz)         #estimated value
+xHatMinus = np.zeros(sz)    #predicted value
+PMinus = np.zeros(sz)       #covariance matrix between predicted and real values
 
 P = np.array([[0, 0], [0, 0]])
 K = np.zeros([2, 1])
@@ -33,8 +33,9 @@ for k in range(9, C_n):
     xHatMinus[:, k] = np.dot(A, xHat[:, k-1]) + np.dot(B, C_g)
     PMinus = np.dot(np.dot(A, P), np.matrix.transpose(A)) + Q
     #
-    K = np.dot(np.dot(PMinus, np.matrix.transpose(H)), 1./(np.dot(np.dot(H, PMinus), np.matrix.transpose(H)) + R))
-    xHat[:, k] = xHatMinus[:, k] + np.dot(K, (z[k] - np.dot(H, xHatMinus[:, k])))
+    K = np.dot(np.dot(PMinus, np.matrix.transpose(H)),
+               1./(np.dot(np.dot(H, PMinus), np.matrix.transpose(H)) + R))
+    xHat[:, k] = xHatMinus[:, k] + np.dot(K, z[k]-np.dot(H, xHatMinus[:, k]))
     P = np.dot(I - np.dot(K, H), PMinus)
 plt.plot(t, xHat[0,:])
 
